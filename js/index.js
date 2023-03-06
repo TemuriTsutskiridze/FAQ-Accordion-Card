a935462e93243c89b95a572fdd855081a44d2a1d
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-03-06 15:48:30 +0400

    main_container images fix

--- js/index.js
+++ js/index.js
@@ -6,12 +6,15 @@ let answer = document.querySelectorAll(".answer");
 
 arrow.forEach(element => {
     element.addEventListener("click", () => {
+        // answer visibility change
         element.parentElement.nextElementSibling.classList.toggle("show");
         element.parentElement.nextElementSibling.classList.toggle("hide");
 
+        // question font-weight change
         element.previousElementSibling.classList.toggle("question_bold");
         element.previousElementSibling.classList.toggle("question_light");
     
+        // arrow rotation change
         element.classList.toggle("arrow_rotate_clockwise");
         element.classList.toggle("arrow_rotate_coutnerclockwise");
     })
