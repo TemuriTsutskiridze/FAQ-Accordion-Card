e054bb95e80ffcabce8150fa3c96a10b649e8ac4
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-03-07 17:20:53 +0400

    improved functionality

--- js/index.js
+++ js/index.js
@@ -1,21 +1,15 @@
-`use strict`
+const accordion_div = document.querySelectorAll(".accordion_div");
 
-let arrow = document.querySelectorAll(".arrow");
-let answer = document.querySelectorAll(".answer");
-
-
-arrow.forEach(element => {
-    element.addEventListener("click", () => {
-        // answer visibility change
-        element.parentElement.nextElementSibling.classList.toggle("show");
-        element.parentElement.nextElementSibling.classList.toggle("hide");
-
-        // question font-weight change
-        element.previousElementSibling.classList.toggle("question_bold");
-        element.previousElementSibling.classList.toggle("question_light");
-    
-        // arrow rotation change
-        element.classList.toggle("arrow_rotate_clockwise");
-        element.classList.toggle("arrow_rotate_coutnerclockwise");
-    })
+let active = null
+accordion_div.forEach(container => {
+    container.addEventListener("click", function (event) {
+        if (event.target.matches(".arrow") || event.target.matches(".question"))
+        {
+            if (active && active != container) {
+                active.classList.remove("show_answer");
+            }
+            container.classList.toggle("show_answer");
+            active = container;
+        }
+    }); 
 });
